5f89dabfbaf94477cb3f0a2f1346c3b21f4e567f
Max Pijittum <mpijittum@fb.com>
2020-01-08 13:40:17 +0800

    :wrench: change config and make nodeapp reload

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, {Application, Request,Response,NextFunction} from 'express';
+import "dotenv/config";
 import bodyParser from 'body-parser';
+import Routes from './routes';
+
 import { removeAllListeners } from 'cluster';
 
 const app: Application = express();
@@ -10,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world');
 });
+
+Routes({ app });
 
 const PORT = process.env.PORT ?? 3000;
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,8 @@
-//import { RoutesInput } from '../types/route';
+import { RoutesInput } from '../types';
+
+
+export default({app} : RoutesInput) => {
+    app.post('api/test', async (req, res) => {
+        return res.send("Test")
+    });
+}
