4136edfc0a2f6d1b55e1519ecaac3ad779ac0a2f
Max Pijittum <mpijittum@fb.com>
2020-01-09 19:36:40 +0800

    :sparkles:Add mutilple HtmlWebpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
-const path = require('path'),
+const _ = require('lodash'),
+    path = require('path'),
     webpack = require('webpack'),
     HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let HWPConfig = new HtmlWebpackPlugin({
+    template: __dirname + "/index.html",
+    file: "index.html",
+    inject: "body"
+})
+
+let articles = ['sass-react', 'chart-js', 'copy-right', 'object-literal', 'spread-operator'];
+
+let multiplesFiles = articles.map(function (entryName) {
+    return new HtmlWebpackPlugin({
+        filename: entryName + '.html',
+        template: __dirname + `/articles/{entryName}.html`
+    })
+})
 
 module.exports = {
     mode: 'development',
     entry: {
-        app: ['./src/client/App.tsx', 'webpack-hot-middleware/client'],
+        app: [path.resolve(__dirname, 'src', 'client', 'App.tsx'), 'webpack-hot-middleware/client'],
         vendor: ['react', 'react-dom']
     },
     output: {
@@ -33,11 +49,11 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                  'style-loader',
-                  'css-loader',
-                  'sass-loader'
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
                 ]
-              },
+            },
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
@@ -45,7 +61,16 @@ module.exports = {
         fs: "empty"
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'client', 'index.html') }),
+        new HtmlWebpackPlugin({ 
+            template: path.resolve(__dirname, 'src', 'client', 'templates', 'master.html'), 
+            filename: 'index.html', 
+            chunks: ['app', 'vendor'] 
+        }),
+        new HtmlWebpackPlugin({ 
+            template: path.resolve(__dirname, 'src', 'client', 'templates', 'login.html'), 
+            filename: 'login.html', 
+            chunks: ['app', 'vendor'] 
+        }),
         new webpack.HotModuleReplacementPlugin()
     ]
 }
