c775ed3ab79fa2a6d879d8cbc84b2064564968d0
Max Pijittum <mpijittum@fb.com>
2020-01-09 22:19:38 +0800

    :sparkles: Add backend connect

--- src/client/components/Hello.tsx
+++ src/client/components/Hello.tsx
@@ -1,14 +1,35 @@
 import * as React from 'react';
 import './Hello.scss'
 
-interface IProps {
+export interface IAppProps {
     compiler: string,
     framework: string,
     bundler: string
 }
 
-export class Hello extends React.Component<IProps, {}> {
+export interface IAppState {
+	name: string;
+}
+
+export class Hello extends React.Component<IAppProps, IAppState> {
+    constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+			name: null
+		};
+	}
+
+    async componentDidMount() {
+		try {
+			let r = await fetch('/api/test');
+            let data = await r.json();
+			this.setState({ ...data });
+		} catch (error) {
+			console.log(error);
+		}
+    }
+    
     render() {
-        return <h1>This is a <area shape="" coords="" href="" alt=""/> {this.props.framework} application using {this.props.compiler} with {this.props.bundler}</h1>
+    return <h1>Hello {this.state.name}. This is a <area shape="" coords="" href="" alt=""/> {this.props.framework} application using {this.props.compiler} with {this.props.bundler}</h1>
     }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -4,20 +4,29 @@ import path from 'path';
 const port = process.env.PORT || 5000
 
 if (typeof process.env.NODE_ENV === 'undefined') {
-  process.env.NODE_ENV = 'production';
+    process.env.NODE_ENV = 'production';
 }
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 const app = express();
 
 if (isDevelopment) {
-    wds(app);     
-  } else {
+    wds(app);
+} else {
     app.use(path.resolve(__dirname, '..', '..', 'dist'), express.static(path.resolve(__dirname, '..', '..', 'dist')));
-  }
+}
+
+app.get("/api", (req: express.Request, res: express.Response): void => {
+    res.send("You have reached the API!");
+});
+
+app.get("/api/test", (req: express.Request, res: express.Response): void => {
+    res.send({name:"Max"});
+});
+
 
 
 
 app.listen(port, () => {
-  console.log(`Server started on port: ${port}, environment: ${process.env.NODE_ENV}, Development: ${isDevelopment}`);
+    console.log(`Server started on port: ${port}, environment: ${process.env.NODE_ENV}, Development: ${isDevelopment}`);
 });
